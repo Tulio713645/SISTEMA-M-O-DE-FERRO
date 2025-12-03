f6cd9687fc760d5b3d5ef4ecfa0d727c1796d6ea
Túlio <tuliobastos713@gmail.com>
2025-12-03 06:25:59 -0300

    fiz uma parte ja

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputDia = document.querySelector("#dia")
+
+const btnEnviar = document.querySelector("#btnEnviar")
+
+const pResultado = document.querySelector("#resultado")
+
+function enviar() {
+    const dia = parseInt(inputDia.value)
+
+    if (dia == 1) {
+        p
+    }
+}
+
+btnEnviar.addEventListener("click", enviar)
