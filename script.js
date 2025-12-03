6031e61a8d0614aecf5e50a327e205d65faef08f
Túlio <tuliobastos713@gmail.com>
2025-12-03 15:25:47 -0300

    codigo concluido

--- script.js
+++ script.js
@@ -7,9 +7,49 @@ const pResultado = document.querySelector("#resultado")
 function enviar() {
     const dia = parseInt(inputDia.value)
 
+    let patente = ""
+
     if (dia == 1) {
-        p
+        patente = "Pessoa Normal"
+    } else if (dia == 2) {
+        patente = "Pessoa Incomum"
+    } else if (dia == 3) {
+        patente = "Já descarreguei Tudo"
+    } else if (dia >= 4 && dia <= 6) {
+        patente = "Já tá só a Carne"
+    } else if (dia >= 7 && dia <= 10) {
+        patente = "Não Aguento mais"
+    } else if (dia == 11) {
+        patente = "Soldado 1"
+    } else if (dia == 12) {
+        patente = "Soldado 2"
+    } else if (dia == 13) {
+        patente = "Soldado 3"
+    } else if (dia >= 14 && dia <= 21) {
+        patente = "Cadê o Jamanta?"
+    } else if (dia == 22) {
+        patente = "Persistente Bronze"
+    } else if (dia == 23) {
+        patente = "Persistente Prata"
+    } else if (dia == 24) {
+        patente = "Persistente Ouro"
+    } else if (dia == 25) {
+        patente = "Só o Vento"
+    } else if (dia == 26) {
+        patente = "Fuzileiro"
+    } else if (dia == 27) {
+        patente = "Broxou"
+    } else if (dia == 28) {
+        patente = "Descabela Broxado"
+    } else if (dia == 29) {
+        patente = "Sou Destransante"
+    } else if (dia == 30) {
+        patente = "Jamanta pré-sigma"
+    } else if (dia == 31) {
+        patente = "Jamanta Sigma"
     }
+
+    pResultado.innerHTML `Patente: ${patente}`
 }
 
 btnEnviar.addEventListener("click", enviar)
